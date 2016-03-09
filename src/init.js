3156177a8eec4839af3b5a7d3740e5fc6917a4a5
somethiiing <wilsonweichengyu@gmail.com>
2016-03-08 19:47:01 -0800

    Include downloaded images, and fix the auto win function

--- src/init.js
+++ src/init.js
@@ -4,25 +4,10 @@ $(document).ready(function() {
 
 
   var XWingNode = new makeBlinkyDancer(100, 100, Math.random() * 2000,
-    '<img class="XWing" src="http://vignette4.wikxxxia.nocookie.net/starwars/images/3/3d/X-wing_Ultimate.png/revision/latest?cb=20150508162001" width="128" height="128">'
+    '<img class="XWing" src="img/xwing.png" width="128" height="128">'
   );
 
   $('body').append(XWingNode.$node);
-
-  // $('.addXWing').on('click', function(event) {
-  //   //var dancerMakerFunctionName = $(this).data('XWing-maker-function-name');
-  //   var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-  //   var dancerMakerFunction = window[dancerMakerFunctionName];
-
-  //   var dancer = new dancerMakerFunction(
-  //     $("body").height() * Math.random(),
-  //     $("body").width() * Math.random(),
-  //     Math.random() * 2000,
-  //     '<img class="XWing" src="http://vignette4.wikxxxia.nocookie.net/starwars/images/3/3d/X-wing_Ultimate.png/revision/latest?cb=20150508162001" width="128" height="128">'
-  //   );
-  //   XWingArray.push(dancer);
-  //   $('body').append(dancer.$node);
-  // });
 
   $('.addDeathStar').on('click', function(event) {
     var dancerMakerFunctionName = $(this).data('DeathStar-maker-function-name');
@@ -32,7 +17,7 @@ $(document).ready(function() {
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000,
-      '<img class="DeathStar" src="http://www.clubpenguinwiki.info/static/images/www/thumb/8/88/DeathStarPlanet.PNG/200px-DeathStarPlanet.PNG" width="128" height="128">'
+      '<img class="DeathStar" src="img/deathstar.png" width="128" height="128">'
     );
     DeathStarArray.push(dancer);
     $('body').append(dancer.$node);
@@ -44,18 +29,7 @@ $(document).ready(function() {
         top: 750,
         left: i * 128
       };
-      debugger;
       DeathStarArray[i].$node.css(styleSettings);
-    }
-
-    for(var i = 0; i < XWingArray.length; i++){
-      var styleSettings = {
-        top: 30,
-        left: i * 128
-      };
-      XWingArray[i].$node.clearQueue();
-      XWingArray[i].$node.css(styleSettings); 
-
     }
   });
 
@@ -67,30 +41,24 @@ $(document).ready(function() {
 
 
   $('.shoot').on('click',function(){
-      
-    
-
     for(var i = 0; i < DeathStarArray.length; i++){
-      var shot = $('<img class="shot" src="http://static.omelete.uol.com.br/static/star_wars/tiiro.png" width="128" height="128">');
+      var explosion = $('<img class="explosion" src="img/explosion.gif" width="128" height="128">');
+      var shot = $('<img class="shot" src="img/laser.png" width="128" height="128">');
       $('body').append(shot);
       var positionXwing = XWingNode.$node.position();
       var positionDeath = DeathStarArray[i].$node.position();
-      shot.css({left: positionXwing.left, top: positionXwing.top},1000);
-      shot.animate({left: positionDeath.left, top: positionDeath.top}, 1000);
+      shot.css({left: positionXwing.left, top: positionXwing.top});
+      shot.animate({left: positionDeath.left, top: positionDeath.top}, 500);
       $(shot).fadeToggle();
+      explosion.css({left: positionDeath.left, top: positionDeath.top});
+      $('body').append(explosion);
+      $(DeathStarArray[i].$node).remove();
+      setTimeout(explosion.remove.bind(explosion), 800);
     }
   });
 
-  // $('body').on('click','.XWing',function(){
-  //   $(this).animate({top: "-=500"},2000);
-  //   $(this).animate({left: "+=500"},2000);
-  //   $(this).animate({top: "+=500"},2000);
-  //   $(this).animate({left: "-=500"},2000);
-  // });
 
-  //http://static.omelete.uol.com.br/static/star_wars/tiiro.png
-
-  $('body').on('click','.XWing',function(){
+  $('body').on('click','.XWing', function() {
     var start = 0;
     var y = 0;
     var x = 0;
