d39f7de5e0f176187da82d65ce9b6a091685ce3f
somethiiing <wilsonweichengyu@gmail.com>
2016-03-08 17:07:26 -0800

    Implement moving xwing with arrow keys + firing with space bar.

--- src/init.js
+++ src/init.js
@@ -1,21 +1,28 @@
 $(document).ready(function() {
-  window.XWingArray = [];
   window.DeathStarArray = [];
+  window.numShot = 0;
 
-  $('.addXWing').on('click', function(event) {
-    //var dancerMakerFunctionName = $(this).data('XWing-maker-function-name');
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-    var dancerMakerFunction = window[dancerMakerFunctionName];
 
-    var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
-      Math.random() * 2000,
-      '<img class="XWing" src="http://vignette4.wikxxxia.nocookie.net/starwars/images/3/3d/X-wing_Ultimate.png/revision/latest?cb=20150508162001" width="128" height="128">'
-    );
-    XWingArray.push(dancer);
-    $('body').append(dancer.$node);
-  });
+  var XWingNode = new makeBlinkyDancer(100, 100, Math.random() * 2000,
+    '<img class="XWing" src="http://vignette4.wikxxxia.nocookie.net/starwars/images/3/3d/X-wing_Ultimate.png/revision/latest?cb=20150508162001" width="128" height="128">'
+  );
+
+  $('body').append(XWingNode.$node);
+
+  // $('.addXWing').on('click', function(event) {
+  //   //var dancerMakerFunctionName = $(this).data('XWing-maker-function-name');
+  //   var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+  //   var dancerMakerFunction = window[dancerMakerFunctionName];
+
+  //   var dancer = new dancerMakerFunction(
+  //     $("body").height() * Math.random(),
+  //     $("body").width() * Math.random(),
+  //     Math.random() * 2000,
+  //     '<img class="XWing" src="http://vignette4.wikxxxia.nocookie.net/starwars/images/3/3d/X-wing_Ultimate.png/revision/latest?cb=20150508162001" width="128" height="128">'
+  //   );
+  //   XWingArray.push(dancer);
+  //   $('body').append(dancer.$node);
+  // });
 
   $('.addDeathStar').on('click', function(event) {
     var dancerMakerFunctionName = $(this).data('DeathStar-maker-function-name');
@@ -34,7 +41,7 @@ $(document).ready(function() {
   $('.LineUp').on('click', function(event) {
     for(var i = 0; i < DeathStarArray.length; i++){
       var styleSettings = {
-        top: 30,
+        top: 750,
         left: i * 128
       };
       debugger;
@@ -43,7 +50,7 @@ $(document).ready(function() {
 
     for(var i = 0; i < XWingArray.length; i++){
       var styleSettings = {
-        top: 750,
+        top: 30,
         left: i * 128
       };
       XWingArray[i].$node.clearQueue();
@@ -58,12 +65,30 @@ $(document).ready(function() {
     
   });
 
+
+  $('.shoot').on('click',function(){
+      
+    
+
+    for(var i = 0; i < DeathStarArray.length; i++){
+      var shot = $('<img class="shot" src="http://static.omelete.uol.com.br/static/star_wars/tiiro.png" width="128" height="128">');
+      $('body').append(shot);
+      var positionXwing = XWingNode.$node.position();
+      var positionDeath = DeathStarArray[i].$node.position();
+      shot.css({left: positionXwing.left, top: positionXwing.top},1000);
+      shot.animate({left: positionDeath.left, top: positionDeath.top}, 1000);
+      $(shot).fadeToggle();
+    }
+  });
+
   // $('body').on('click','.XWing',function(){
   //   $(this).animate({top: "-=500"},2000);
   //   $(this).animate({left: "+=500"},2000);
   //   $(this).animate({top: "+=500"},2000);
   //   $(this).animate({left: "-=500"},2000);
   // });
+
+  //http://static.omelete.uol.com.br/static/star_wars/tiiro.png
 
   $('body').on('click','.XWing',function(){
     var start = 0;
@@ -76,7 +101,48 @@ $(document).ready(function() {
       $(this).animate({top: "+="+y, left: "+="+x},100);
     }
   });
+  
 
+  $("body").keydown(function(event) { 
+    var $mover = $(".XWing");
+
+    var addTop = function(diff) {
+      $mover.css("top", ($mover.position().top + diff) + "px"); 
+      //if using tracked position:
+      //   top += diff;
+      //   $mover.css("top", top) + "px");
+    };
+
+    var addLeft = function(diff) {
+      $mover.css("left", ($mover.position().left + diff) + "px");
+      //if using tracked position:
+      //   left += diff;
+      //   $mover.css("left", left) + "px");
+    };
+
+    var fireShot = function(){
+      var shot = $('<img class="shot" src="http://static.omelete.uol.com.br/static/star_wars/tiiro.png" width="128" height="128">');
+      $('body').append(shot);
+
+      var positionXwing = XWingNode.$node.position();
+      shot.css({left: positionXwing.left, top: positionXwing.top},1000);
+      shot.animate({left: "+=200", top: "+=200"}, 1000);
+      $(shot).fadeToggle();
+    };
+
+    switch(event.keyCode) {
+    case 37: //left
+      addLeft(-15); break; 
+    case 38: //up
+      addTop(-15); break;
+    case 39: //right
+      addLeft(15); break;
+    case 40: //down
+      addTop(15); break;
+    case 32:
+      fireShot();
+    }
+  });
 
 });
 
